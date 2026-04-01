c62de4e2aa73df614ecb53e8d4332d0a09f06254
Keith Packard <keithp@keithp.com>
2026-04-01 16:14:09 -0700

    Version 1.9.22.1 (code 38)
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- app/src/androidTest/java/org/altusmetrum/altosdroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/altusmetrum/altosdroid/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.altusmetrum.altosdroid", appContext.getPackageName());
+        assertEquals("org.altusmetrum.AltosDroid", appContext.getPackageName());
     }
 }
